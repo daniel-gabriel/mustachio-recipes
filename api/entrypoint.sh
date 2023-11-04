898c48149b62439703d087fac660a3101f612128
dan@keshigomi.com <dan@keshigomi.com>
2023-11-04 10:21:48 -0400

    added backslash unescaping to the entrypoint.sh script for api vars

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -2,6 +2,7 @@
 
 node <<EOF
 const fs = require("fs");
+console.log("firebase key: " + process.env.FIREBASE_PRIVATE_KEY);
 const template = JSON.parse(fs.readFileSync("/usr/src/app/firebase-adminsdk-auth-key.template.json", "utf8"));
 template.project_id = process.env.FIREBASE_PROJECT_ID;
 template.private_key_id = process.env.FIREBASE_PRIVATE_KEY_ID;
@@ -9,7 +10,7 @@ template.private_key = process.env.FIREBASE_PRIVATE_KEY;
 template.client_email = process.env.FIREBASE_CLIENT_EMAIL;
 template.client_id = process.env.FIREBASE_CLIENT_ID;
 template.client_x509_cert_url = process.env.FIREBASE_CLIENT_X509_CERT_URL;
-fs.writeFileSync("/usr/src/app/firebase-adminsdk-auth-key.json", JSON.stringify(template, null, 4));
+fs.writeFileSync("/usr/src/app/firebase-adminsdk-auth-key.json", JSON.stringify(template, null, 4).replace("\\n", "\n"));
 EOF
 
 cat /usr/src/app/firebase-adminsdk-auth-key.json
